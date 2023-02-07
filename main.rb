fd3bf2bb8dd39c7761c715f5fe138fbd7ecb3acc
kevinkkimutai <kiruikevinkim@gmail.com>
2023-02-07 11:43:21 +0300

    day1

--- main.rb
+++ main.rb
@@ -2,36 +2,58 @@
 
 # 1.0 Printing Methods
 # TODO: puts
+puts "Hello Ruby puts"
 
 # TODO: print
-
+print "Hello Ruby print"
 # TODO: pp
+pp [1,2,3,4]
+# TODO: p
+p "Hello Ruby p"
 
 # 2.0 Strings
 # TODO: CREATE A STRING
 
+ last_name = "Kirui"
+
+
 # TODO: CREATE A STRING WITH SINGLE QUOTES
+first_name = 'Kevin'
 
 # TODO: INTERPOLATE STRINGS
+full_name = "First Name: #{first_name}.last Name: #{last_name}"
 
 # TODO: String methods (upcase, downcase, capitalize)
+puts full_name
+puts full_name.upcase
+puts full_name.capitalize
+puts full_name.downcase
+
 
 # 3.0 Numbers
 # TODO: Integers
+height = 100
 
 # TODO: Float
+temp = 55.56879
 
 # TODO: Number methods (floor, ceil, to_i, to_f)
-
+puts temp.floor(1)
+puts temp.ceil
+puts temp.to_i
+puts temp.to_f
 
 # 4.0 Boolean
 # TODO: True / Truthy
+isMonday = true
 
 # TODO: False / Falsy
+isTuesday = false
 
 # 5.0 Symbols
 # TODO: Create symbol
-
+:name 
+puts :name
 
 # 6.0 Arrays
 # TODO: Create array natively
@@ -40,14 +62,19 @@
 
 # 7.0 Hashes
 # TODO: Create Hash
-
+{name, age, gender}
 # TODO: Create Hash using Hash class
 
 # 8.0 Methods
 # TODO: Method with enclosed params
+def method_name (name)
+  
+end
 
 # TODO: Method with open params
-
+def method_age age
+    
+end
 
 
 
